7e3ae4512a97483e569d0537094615336ad7635e
Joe Messina <giuseppe91messina@gmail.com>
2019-04-12 00:58:56 +0300

    Update script.js
    better, cleaner, almost done

--- js/script.js
+++ js/script.js
@@ -10,7 +10,22 @@ FSJS project 2 - List Filter and Pagination
 const list_items = document.querySelector('.student-list').children;
 
 // 'items_per_page' holds the number of lis we want to show per page
-const items_per_page = 10;
+const items_per_page = 5;
+
+const divPage = document.querySelector('.page');
+
+const page_header = document.querySelector('.page-header');
+
+const studentList = document.querySelector('.student-list');
+const lis_students = studentList.children;
+
+// The following lines create, append and hide the message that
+// is displayed to the page if no student is found with the search.
+const message = document.createElement('h2');
+message.textContent = 'No students found!';
+message.style.fontWeight = 'lighter';
+divPage.insertBefore(message, studentList);
+message.style.display = 'none';
 
 
 /***
@@ -42,16 +57,19 @@ const showPage = (list, page) => {
 
 
 /***
-   In the 'appendPageLinks' function we create and append elements
-   to the DOM in order to display a navigation at the buttom of the page.
+   In the 'appendPageLinks' function we create, append and give functionality
+   to a pagination at the buttom of the page.
    the navigation takes the user through the different pages of our app.
+   The function takes a parameter 'list' and our html collection of list items
+   is going to be the argument of the function.
 ***/
 
 const appendPageLinks = (list) => {
-  const divPage = document.querySelector('.page');
+
+// First we create and append the elements to the page
+
   const div = document.createElement('div');
   div.className = 'pagination';
-
   const ul = document.createElement('ul');
 
   let numOfPages = Math.ceil(list.length / items_per_page);
@@ -64,64 +82,63 @@ const appendPageLinks = (list) => {
 
     li.appendChild(a);
     ul.appendChild(li);
+
   }
   div.appendChild(ul);
   divPage.appendChild(div);
-}
 
-appendPageLinks(list_items);
+  // At this point in the function the pagination is in place.
+  // now we target the first link in the navigation and assign a className of
+  // '.active' to it.
 
+  const first_li = ul.getElementsByTagName('li')[0];
+  const first_active_a = first_li.firstElementChild;
+  first_active_a.className = 'active';
 
+/***
+  Now we loop through all the link elements we just created
+  and add a 'click' event listener to all of them.
+  we loop through them one more time and remove the className of each one of them.
+  then we assign a className of 'active' only to the event targeted link only.
 
-// we target elements in our index.html and give a 'className' of
-// '.active' to the first link in the navigation at the buttom of the page.
+  Finally we call the 'showPage' function.
+  Its arguments:
+  1) the same that we'll pass to the 'appendPageLinks' function, which is
+  our html collection
+  2) and the last targeted link's textContent.
+  ***/
 
-    const div_pagination = document.querySelector('.pagination');
-    const ul_pagination = div_pagination.getElementsByTagName('ul')[0];
-    const first_li = ul_pagination.getElementsByTagName('li')[0];
-    const first_active_a = first_li.firstElementChild;
-    first_active_a.className = 'active';
+  const lis_pagination = ul.children;
 
+  for(let i = 0; i < lis_pagination.length; i += 1) {
+    let li = lis_pagination[i];
+    let a = li.firstElementChild;
+    a.addEventListener('click', (e) => {
 
+      for(let i = 0; i < lis_pagination.length; i += 1) {
+        let li = lis_pagination[i];
+        let a = li.firstElementChild;
+        a.className = '';
+        e.target.className = 'active';
+      }
 
-// we loop through the a elements we just created with 'appendPageLinks'
-// and add a 'click' event listener to all of them.
-// we loop through them one more time and remove the className of each one of them.
-// then we assign a className of 'active' only to the event targeted link only.
-// Finally we call the 'showPage' function passing our html collection and
-// the last targeted link textContent as arguments.
-
-const lis_pagination = ul_pagination.children;
-
-for(let i = 0; i < lis_pagination.length; i += 1) {
-  let li = lis_pagination[i];
-  let a = li.firstElementChild;
-  a.addEventListener('click', (e) => {
-
-    for(let i = 0; i < lis_pagination.length; i += 1) {
-      let li = lis_pagination[i];
-      let a = li.firstElementChild;
-      a.className = '';
-      e.target.className = 'active';
-    }
-
-  showPage(list_items, e.target.textContent);
-  });
+    showPage(list, e.target.textContent);
+    });
+  }
 }
 
 
 // We call the 'showPages' function passing the html collection and
-// the number 1 as arguments to display the first page of the first 10 students as
+// the number 1 as arguments to display the first page with the first 10 students as
 // a starting point for our app.
 
 showPage(list_items, 1);
-
+appendPageLinks(list_items);
 
 
 // in the following lines we create and manipulate some elements
 // in order to append a search bar in the top right corner of our app.
 
-const page_header = document.querySelector('.page-header');
 const search_div = document.createElement('div');
 search_div.className = 'student-search';
 const search_input = document.createElement('input');
@@ -135,54 +152,48 @@ page_header.appendChild(search_div);
 
 
 
-// THAT'S WHERE THE PROBLEMS BEGIN :)
-// -----------------------------
 
-const studentList = document.querySelector('.student-list');
-const lis_students = studentList.children;
-
-
-// add keyup event listener to search input
+// We add a 'keyup' event listener to the search bar and filter
+// the displaying students by comparing the user input with our
+// students names.
 
 search_input.addEventListener('keyup', () => {
   let input = search_input;
   let inputValue = input.value.toUpperCase();
-//  let inputLength = inputValue.length;
-  let namesArray = []; // --------------------------------------
+  let namesArray = [];
 
     for(let i = 0; i < lis_students.length; i += 1) {
       let li =  lis_students[i];
       let h3 = li.getElementsByTagName('h3')[0];
       let name = h3.textContent.toUpperCase();
-//      let nameSubStr = name.substring(0, inputLength);
 
       if(name.includes(inputValue)) {
         li.style.display = '';
-        namesArray.push(li);  // ------------------------------
+        namesArray.push(li);
       }else{
         li.style.display = 'none';
       }
     }
 
-    // appendPageLinks(namesArray);
-    // document.querySelector('.pagination').style.display = 'none';
+// with the help of the array we build during our looping
+// and the following if/else statement we display a message on the
+// page when no students are found during the search.
 
+  if(namesArray.length === 0) {
+    message.style.display = '';
+  }else{
+    message.style.display = 'none';
+  }
 
-// not sure about the following
-
-    if(inputValue === '') {
-      showPage(list_items, 1);
-    }
-    // else{
-    //   document.querySelector('.pagination').remove();
-    //   appendPageLinks(list_items);
-    //   showPage(list_items, 1);
-    // }
-
+  document.querySelector('.pagination').remove();
+  showPage(namesArray, 1);
+  appendPageLinks(namesArray);
 })
 
 
-// add 'click' event listener to search button
+
+// We add a 'click' event listener to the search button
+// SOMETHING WRONG HERE 
 
 search_button.addEventListener('click', () => {
   let input = search_input;
@@ -202,6 +213,5 @@ search_button.addEventListener('click', () => {
         li.style.display = 'none';
       }
     }
-  input.value = '';
   }
 })
